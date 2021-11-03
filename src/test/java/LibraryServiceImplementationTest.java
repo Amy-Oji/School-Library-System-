3047c4335048ddef640e7a1f4f8af3daf5efedc7
Amy-Oji <amyojiakor@gmail.com>
2021-11-03 15:57:04 +0100

    updated2

--- src/test/java/LibraryServiceImplementationTest.java
+++ src/test/java/LibraryServiceImplementationTest.java
@@ -1,4 +1,5 @@
 import enums.BooksCollection;
+import enums.PersonStatus;
 import models.Books;
 import models.Person;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +14,7 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryServiceImplementationTest {
+    LibraryServiceImplementation lib;
     private Books books;
     private Person person;
     private LibraryService libraryService;
@@ -25,16 +27,29 @@ class LibraryServiceImplementationTest {
         libraryService = new LibraryServiceImplementation();
         personServices = new LibraryServiceImplementation();
         booksCatalogue = new HashMap<>();
+        lib = new LibraryServiceImplementation();
     }
 
     @Test
     void addBookToShelf() {
+        libraryService.addBookToShelf(books, 3);
+
+        booksCatalogue = libraryService.getBooksCatalogue();
+
+        assertNotNull(booksCatalogue);
+
+        assertEquals(3, booksCatalogue.get(books.getBooksCollection().name()));
 
     }
 
 
     @Test
     void giveBookToPerson() {
+        Person juniorStudent3 = new Person("Francis", "89574", PersonStatus.JUNIOR_STUDENT);
+        Person teacher1 = new Person("Daro", "67480", PersonStatus.TEACHER);
+        lib.getPriorityQueue().add(juniorStudent3);
+        lib.getPriorityQueue().add(teacher1);
+        assertEquals(teacher1.getName(), lib.getPriorityQueue().remove().getName());
     }
 
     @Test
